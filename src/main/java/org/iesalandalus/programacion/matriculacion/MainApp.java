72c0f858d4911d191ca140d53202869d27c9b4b5
Manuel <mlc280803@gmail.com>
2025-05-03 13:37:27 +0200

    Modificada la clase MainApp.

--- src/main/java/org/iesalandalus/programacion/matriculacion/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/matriculacion/MainApp.java
@@ -1,6 +1,7 @@
 package org.iesalandalus.programacion.matriculacion;
 
 import org.iesalandalus.programacion.matriculacion.controlador.Controlador;
+import org.iesalandalus.programacion.matriculacion.modelo.FactoriaFuenteDatos;
 import org.iesalandalus.programacion.matriculacion.modelo.Modelo;
 import org.iesalandalus.programacion.matriculacion.vista.Vista;
 
@@ -9,11 +10,26 @@ public class MainApp {
     public static void main(String[] args) {
         System.out.println("Iniciando la aplicación...");
 
-        Modelo modelo = new Modelo();
+        Modelo modelo = procesarArgumentosFuenteDatos(args);
         Vista vista = new Vista();
         Controlador controlador = new Controlador(modelo, vista);
         controlador.comenzar();
 
         System.out.println("Aplicación cerrada.");
+    }
+
+    private static Modelo procesarArgumentosFuenteDatos(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("ERROR: Debe especificarse una fuente de datos (-fdmemoria o -fdmysql).");
+        }
+
+        switch (args[0].toLowerCase()) {
+            case "-fdmemoria":
+                return new Modelo(FactoriaFuenteDatos.MEMORIA);
+            case "-fdmysql":
+                return new Modelo(FactoriaFuenteDatos.MYSQL);
+            default:
+                throw new IllegalArgumentException("ERROR: Parámetro de fuente de datos no válido. Use -fdmemoria o -fdmysql.");
+        }
     }
 }
